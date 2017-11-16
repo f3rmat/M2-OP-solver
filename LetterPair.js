ef111f1df8f7db3030a4cb4e2af635ef4a6acbf5
= <hemangsarkar@gmail.com>
2017-11-16 20:18:44 +0530

    Started adding letter pair words.

---
+++ LetterPair.js
@@ -0,0 +1,8 @@
+var LetterPair = ["test", 
+"Aardvark", "Abs", "Airconditioniner", "Advertisement", "Aeroplane", "Air Force", "Alligator",
+"Adolf Hitler", "Artificial Intelligence", "Angelina Jolie", "AK-47", "Alien", "Ambulance",
+"Ant", "Aosu", "Apple", "Aqua", "Arrow", "Astronaut", "Atlas", "Automobile", "Avenue", "AWake",
+"Axe",
+
+
+];
