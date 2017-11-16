f3785c8af9ac2f65277c3d2f788fe9ad32fd26dc
= <hemangsarkar@gmail.com>
2017-11-16 20:38:53 +0530

    Added B, C and D.

--- LetterPair.js
+++ LetterPair.js
@@ -4,5 +4,18 @@ var LetterPair = ["test",
 "Ant", "Aosu", "Apple", "Aqua", "Arrow", "Astronaut", "Atlas", "Automobile", "Avenue", "AWake",
 "Axe",
 
+"Batman", "Basketball", "Bicycle", "Bed", "Bee", "Boyfriend", "Badge", "Beehive", "Bird", "Bad Job",
+"Book", "Blood", "Bomb", " Banana", "Bone", "Backpack", "Barbeque", "Bear", "BullShit", "Boots",
+"Bus", "Beaver", "Bow", "Box",
+
+"Camera", "Cube", "Capital Cubing", "CD", "Cement", "Coffin", "Cigarette", "Chair", "City",
+"Cage", "Cake", "Clown", "Comb", "Cone", "Coke", "Cap", "Croquet", "Cristiano Ronaldo", "Chess", "Cat",
+"Cup", "Cave", "Cow", "Cyoubx",
+
+"Dagger", "Dobby", "Dice", "Dad", "Dentist", "Dolphin", "Dog", "Dough", "Diglett", "DJ", "Duck",
+"Doll", "Doom", "Donut", "Dope", "Diaper", "Disqualified", "Dragon", "Dinosaur", "Dart", " Dunkin doughnuts",
+"Darth Vader", "Dwarf", "Ducks",
+
+
 
 ];
